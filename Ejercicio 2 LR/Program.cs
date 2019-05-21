f7a9886a23b9a87f5ceb66e5f9037247a98f4f42
LeonardoRios18 <LeonardoRios_Beta18@users.noreply.github.com>
2019-05-20 20:35:49 -0500

    Initial commit

---
+++ Ejercicio 2 LR/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_LR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, password;
+            string usuario = "sisadmin";
+            string contraseña = "admin3.0";
+            string pregunta;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Digite su usuario");
+                login = Console.ReadLine();
+
+                Console.WriteLine("Digite su contraseña");
+                password = Console.ReadLine();
+
+                if ((login == usuario) && (password == contraseña))
+                {
+                    Console.WriteLine($"Bienvenido {login}");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine($"Los datos son incorrectos \n{login} {password} no coinciden");
+
+                }
+
+
+
+            }
+
+            Console.WriteLine("¿Quieres cambiar tu contraseña? s/n");
+            pregunta = Console.ReadLine();
+            if (pregunta == "s")
+            {
+                Console.WriteLine("Digita nueva contrasena");
+                contraseña = Console.ReadLine();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Digite su usuario");
+                    login = Console.ReadLine();
+
+                    Console.WriteLine("Digite su contrasena");
+                    password = Console.ReadLine();
+
+                    if ((login == usuario) && (password == contraseña))
+                    {
+                        Console.WriteLine("Bienvenido");
+                        break;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Los datos son incorrectos");
+                    }
+
+                }
+
+
+            }
+            else
+            {
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
